aa47ea57e66fbb509bb7530982c1b96a891d8623
apoyet <32321729+apoyet@users.noreply.github.com>
2020-04-30 13:40:12 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='bbcw2mad',
+      version='0.01',
+      description='TODO',
+      url='https://github.com/apoyet/bbcw2mad',
+      author='Axel Poyet',
+      author_email='axel.poyet@cern.ch',
+      license='MIT',
+      packages=['bbcw2mad'],
+      zip_safe=False)
