e512f85ea1544a327ce3a6528f09ec2324d990fa
Axel Poyet <axel.poyet@cern.ch>
2020-05-07 18:28:08 +0200

    correction parenthesisz

--- bbcw2mad/knob_wires.py
+++ bbcw2mad/knob_wires.py
@@ -46,8 +46,8 @@ class KnobObj():
         kq4_r1_0 = self.mad_inst.eval('kq4.r1b1')
         kq4_l1_0 = self.mad_inst.eval('kq4.l1b1')
         self.mad_inst.input(f'''
-        kq4.l1b1 := -5.339518293867958e-12*(on_{wire_ir1.name}*I_{wire_ir1.name}/{wire_ir1.y_pos**2}) + {kq4_l1_0};
-        kq4.r1b1 := -1.4733791126352925e-12*(on_{wire_ir1.name}*I_{wire_ir1.name}/{wire_ir1.y_pos**2}) + {kq4_r1_0};
+        kq4.l1b1 := -5.339518293867958e-12*(on_{wire_ir1.name}*I_{wire_ir1.name}/{wire_ir1.y_pos**2}) + ({kq4_l1_0});
+        kq4.r1b1 := -1.4733791126352925e-12*(on_{wire_ir1.name}*I_{wire_ir1.name}/{wire_ir1.y_pos**2}) + ({kq4_r1_0});
         ''')
     
     def define_q4_ir5_knob(self, wire_ir5):
@@ -59,6 +59,6 @@ class KnobObj():
         kq4_r5_0 = self.mad_inst.eval('kq4.r5b1')
         kq4_l5_0 = self.mad_inst.eval('kq4.l5b1')
         self.mad_inst.input(f'''
-        kq4.l5b1 := 5.271370021700931e-12*(on_{wire_ir5.name}*I_{wire_ir5.name}/{wire_ir5.x_pos**2}) + {kq4_l5_0};
-        kq4.r5b1 := 1.3545529526911738e-12*(on_{wire_ir5.name}*I_{wire_ir5.name}/{wire_ir5.x_pos**2}) + {kq4_r5_0};
+        kq4.l5b1 := 5.271370021700931e-12*(on_{wire_ir5.name}*I_{wire_ir5.name}/{wire_ir5.x_pos**2}) + ({kq4_l5_0});
+        kq4.r5b1 := 1.3545529526911738e-12*(on_{wire_ir5.name}*I_{wire_ir5.name}/{wire_ir5.x_pos**2}) + ({kq4_r5_0});
         ''')
