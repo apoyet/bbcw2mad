eaa75c658b6412b7a552418c756cd8691cee40ca
apoyet <32321729+apoyet@users.noreply.github.com>
2020-06-17 10:05:25 +0200

    Try zeroing the error on knob for 350 A

--- bbcw2mad/KnobObj.py
+++ bbcw2mad/KnobObj.py
@@ -57,3 +57,29 @@ class KnobObj():
         kq4.l5b2 := 1.3416528333566366e-12*(on_{wire_ir5.name}*I_{wire_ir5.name}/{wire_ir5.x_pos**2}) + ({kq4_l5_0});
         kq4.r5b2 := 5.2307795653834585e-12*(on_{wire_ir5.name}*I_{wire_ir5.name}/{wire_ir5.x_pos**2}) + ({kq4_r5_0});
         ''')
+        
+    def define_q4_ir1_knob_b2_v2(self, wire_ir1):
+        '''
+        Defines the know of the Q4 quadrupoles located in IR1.
+        Args:
+            wire_ir1: WireObj, one of the wires located in IR1
+        '''
+        kq4_r1_0 = self.mad_inst.eval('kq4.r1b2')
+        kq4_l1_0 = self.mad_inst.eval('kq4.l1b2')
+        self.mad_inst.input(f'''
+        kq4.l1b2 := -1.468115939147405e-12*(on_{wire_ir1.name}*I_{wire_ir1.name}/{wire_ir1.y_pos**2}) + ({kq4_l1_0});
+        kq4.r1b2 := -5.3182616702086385e-12*(on_{wire_ir1.name}*I_{wire_ir1.name}/{wire_ir1.y_pos**2}) + ({kq4_r1_0});
+        ''')
+    
+    def define_q4_ir5_knob_b2_v2(self, wire_ir5):
+        '''
+        Defines the know of the Q4 quadrupoles located in IR5.
+        Args:
+            wire_ir5: WireObj, one of the wires located in IR5
+        '''
+        kq4_r5_0 = self.mad_inst.eval('kq4.r5b2')
+        kq4_l5_0 = self.mad_inst.eval('kq4.l5b2')
+        self.mad_inst.input(f'''
+        kq4.l5b2 := 1.3418104464203787e-12*(on_{wire_ir5.name}*I_{wire_ir5.name}/{wire_ir5.x_pos**2}) + ({kq4_l5_0});
+        kq4.r5b2 := 5.2306872719491174e-12*(on_{wire_ir5.name}*I_{wire_ir5.name}/{wire_ir5.x_pos**2}) + ({kq4_r5_0});
+        ''')
