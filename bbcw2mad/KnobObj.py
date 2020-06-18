65570eeba89f2e637de09497b1be009f766302db
apoyet <32321729+apoyet@users.noreply.github.com>
2020-06-18 09:43:01 +0200

    Final version of knobs

--- bbcw2mad/KnobObj.py
+++ bbcw2mad/KnobObj.py
@@ -15,8 +15,8 @@ class KnobObj():
         kq4_r1_0 = self.mad_inst.eval('kq4.r1b1')
         kq4_l1_0 = self.mad_inst.eval('kq4.l1b1')
         self.mad_inst.input(f'''
-        kq4.l1b1 := -5.339518293867958e-12*(on_{wire_ir1.name}*I_{wire_ir1.name}/{wire_ir1.y_pos**2}) + ({kq4_l1_0});
-        kq4.r1b1 := -1.4733791126352925e-12*(on_{wire_ir1.name}*I_{wire_ir1.name}/{wire_ir1.y_pos**2}) + ({kq4_r1_0});
+        kq4.l1b1 := -5.320574727686048e-12*(on_{wire_ir1.name}*I_{wire_ir1.name}/{wire_ir1.y_pos**2}) + ({kq4_l1_0});
+        kq4.r1b1 := -1.4684642611833821e-12*(on_{wire_ir1.name}*I_{wire_ir1.name}/{wire_ir1.y_pos**2}) + ({kq4_r1_0});
         ''')
     
     def define_q4_ir5_knob_b1(self, wire_ir5):
@@ -28,37 +28,11 @@ class KnobObj():
         kq4_r5_0 = self.mad_inst.eval('kq4.r5b1')
         kq4_l5_0 = self.mad_inst.eval('kq4.l5b1')
         self.mad_inst.input(f'''
-        kq4.l5b1 := 5.271370021700931e-12*(on_{wire_ir5.name}*I_{wire_ir5.name}/{wire_ir5.x_pos**2}) + ({kq4_l5_0});
-        kq4.r5b1 := 1.3545529526911738e-12*(on_{wire_ir5.name}*I_{wire_ir5.name}/{wire_ir5.x_pos**2}) + ({kq4_r5_0});
+        kq4.l5b1 := 5.270724254902356e-12*(on_{wire_ir5.name}*I_{wire_ir5.name}/{wire_ir5.x_pos**2}) + ({kq4_l5_0});
+        kq4.r5b1 := 1.3539450288338998e-12*(on_{wire_ir5.name}*I_{wire_ir5.name}/{wire_ir5.x_pos**2}) + ({kq4_r5_0});
         ''')
         
     def define_q4_ir1_knob_b2(self, wire_ir1):
-        '''
-        Defines the know of the Q4 quadrupoles located in IR1.
-        Args:
-            wire_ir1: WireObj, one of the wires located in IR1
-        '''
-        kq4_r1_0 = self.mad_inst.eval('kq4.r1b2')
-        kq4_l1_0 = self.mad_inst.eval('kq4.l1b2')
-        self.mad_inst.input(f'''
-        kq4.l1b2 := -1.4684543600483665e-12*(on_{wire_ir1.name}*I_{wire_ir1.name}/{wire_ir1.y_pos**2}) + ({kq4_l1_0});
-        kq4.r1b2 := -5.3177729704720105e-12*(on_{wire_ir1.name}*I_{wire_ir1.name}/{wire_ir1.y_pos**2}) + ({kq4_r1_0});
-        ''')
-    
-    def define_q4_ir5_knob_b2(self, wire_ir5):
-        '''
-        Defines the know of the Q4 quadrupoles located in IR5.
-        Args:
-            wire_ir5: WireObj, one of the wires located in IR5
-        '''
-        kq4_r5_0 = self.mad_inst.eval('kq4.r5b2')
-        kq4_l5_0 = self.mad_inst.eval('kq4.l5b2')
-        self.mad_inst.input(f'''
-        kq4.l5b2 := 1.3416528333566366e-12*(on_{wire_ir5.name}*I_{wire_ir5.name}/{wire_ir5.x_pos**2}) + ({kq4_l5_0});
-        kq4.r5b2 := 5.2307795653834585e-12*(on_{wire_ir5.name}*I_{wire_ir5.name}/{wire_ir5.x_pos**2}) + ({kq4_r5_0});
-        ''')
-        
-    def define_q4_ir1_knob_b2_v2(self, wire_ir1):
         '''
         Defines the know of the Q4 quadrupoles located in IR1.
         Args:
@@ -71,7 +45,7 @@ class KnobObj():
         kq4.r1b2 := -5.318205936567371e-12*(on_{wire_ir1.name}*I_{wire_ir1.name}/{wire_ir1.y_pos**2}) + ({kq4_r1_0});
         ''')
     
-    def define_q4_ir5_knob_b2_v2(self, wire_ir5):
+    def define_q4_ir5_knob_b2(self, wire_ir5):
         '''
         Defines the know of the Q4 quadrupoles located in IR5.
         Args:
@@ -83,3 +57,5 @@ class KnobObj():
         kq4.l5b2 := 1.3413468940489072e-12*(on_{wire_ir5.name}*I_{wire_ir5.name}/{wire_ir5.x_pos**2}) + ({kq4_l5_0});
         kq4.r5b2 := 5.230871016636163e-12*(on_{wire_ir5.name}*I_{wire_ir5.name}/{wire_ir5.x_pos**2}) + ({kq4_r5_0});
         ''')
+        
+    
